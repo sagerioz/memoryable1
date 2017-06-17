3bf75b824471069152c1427ad381fa389e3530a5
sagerioz <ericaepperson27@yahoo.com>
2017-06-17 11:04:43 -0600

    weather API is returning results

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from './../navbar'
 //import { Link } from 'react-router-dom'
-//import $ from 'jquery'
+import $ from 'jquery'
 
 class Home extends Component {
   constructor(props) {
@@ -11,12 +11,32 @@ class Home extends Component {
       photos: [],
       title: ''
     }
-
-
   }
 
   componentDidMount(){
-    
+    const apiKey = '58443d73bb4adf5b12a65dda8efd13fb'
+    let userData = ''
+    $.ajax({
+      method: 'get',
+      url: `http://api.openweathermap.org/data/2.5/weather?zip=80301,us&appid=${apiKey}`,
+      dataType: 'jsonp',
+      success: function(result) {
+        console.log(result);
+        userData = {
+          cityName: result.name,
+          weatherDesc: result.weather[0].description,
+          temp: result.main.temp
+
+        }
+        console.log("TOWN",userData.cityName);
+        console.log("WEATHER TODAY IS", userData.weatherDesc);
+        console.log("TEMP TODAY IS", userData.temp);
+
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    }).then(() => {
     fetch('/api/scrapbook', {
            method: 'GET'
          }).then(res => {
@@ -29,7 +49,9 @@ class Home extends Component {
             console.log("PICS", pics);
           })
      })
-  }
+   })
+   }
+
 
 
   render() {
