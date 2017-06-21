ca8b498bac87990d4be93d89f896d42231344fef
sagerioz <ericaepperson27@yahoo.com>
2017-06-20 21:23:47 -0600

    fixed date object

--- client/src/components/date.js
+++ client/src/components/date.js
@@ -4,19 +4,34 @@ export class MyDate extends React.Component {
     constructor() {
         super();
 
-        let today = new Date(),
-          date = 'Today is ' + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDay();
+        let today = new Date();
+
+        let dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+         let day = dayOfWeek[today.getDay()]
+
+         var monthNames = ["January", "February", "March", "April", "May", "June",
+         "July", "August", "September", "October", "November", "December"
+          ];
+
+         var date = today.getUTCDate();
+
+         let month = monthNames[today.getMonth()]
+
+         let dateNow = 'Today is ' + day + ', ' + month + ' ' + date + ', ' + today.getFullYear()
+
 
  console.log("DATE", today);
+
         this.state = {
-            date: date
+            dateNow: dateNow
         };
     }
 
     render() {
         return (
             <div>
-              {this.state.date}
+              {this.state.dateNow}
             </div>
         );
     }
