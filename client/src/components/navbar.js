bd814593a0102c852f4aeeff083bcd03ca673c19
sagerioz <ericaepperson27@yahoo.com>
2017-06-22 19:07:04 -0600

    responsive nav bar installed

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -15,28 +15,64 @@ class Navbar extends Component {
   render() {
 
         let guestLinks = (
-          <ul className="nav navbar-nav navbar-right">
-            <li><Link to="/scrapbook">Scrapbook</Link></li>
-            <li><Link to="/news">News</Link></li>
-            <li><Link to="/todos">Todos</Link></li>
-            <li><Logout/></li>
-            <li><Profile/></li>
+//           <nav class="navbar navbar-default navbar-fixed-top">
+//   <div class="container">
+  <ul className="nav navbar-nav navbar-right">
+    <li><Link to="/scrapbook">Scrapbook</Link></li>
+    <li><Link to="/news">News</Link></li>
+    <li><Link to="/todos">Todos</Link></li>
+    <li><Logout/></li>
+    <li><Profile/></li>
 
-          </ul>
-        )
+  </ul>
+//   </div>
+// </nav>
+
+       )
 
         return (
-          <nav className="navbar navbar-default">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <Link to="/" className="navbar-brand">Memoryable</Link>
-              </div>
+          // <nav className="navbar navbar-default">
+          //   <div className="container-fluid">
+          //     <div className="navbar-header">
+          //       <Link to="/" className="navbar-brand">Memoryable</Link>
+          //     </div>
+          //
+          //     <div className="collapse navbar-collapse">
+          //       { guestLinks }
+          //     </div>
+          //   </div>
+          // </nav>
 
-              <div className="collapse navbar-collapse">
-                { guestLinks }
+
+
+          <nav className="navbar navbar-default navbar-static-top">
+              <div className="container">
+
+
+                  <div className="navbar-header">
+                      <button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle">
+                          <span className="sr-only">Toggle navigation</span>
+                          <span className="icon-bar"></span>
+                          <span className="icon-bar"></span>
+                          <span className="icon-bar"></span>
+                      </button>
+                      <a href="#" clasNames="navbar-brand">Brand</a>
+                  </div>
+
+                  <div id="navbarCollapse" className="collapse navbar-collapse">
+                    { guestLinks }
+                  </div>
               </div>
-            </div>
           </nav>
+
+
+
+
+
+
+
+
+
         );
       }
     }
