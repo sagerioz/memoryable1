427b2ade18021c4e9c93d4b6bfdcd8f906508b7a
sagerioz <ericaepperson27@yahoo.com>
2017-06-25 00:05:26 -0600

    more login parallax work

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -28,16 +28,60 @@ class Login extends Component {
   render() {
 
 
+      // return (
+      //     <div className="row">
+      //     <div className="parallax"></div>
+      //     <div></div>
+      //      <div>
+      //      <SignupForm /><LoginForm />
+      //      <div className="bg-img2"></div><About />
+      //      <div className="bg-img5"></div>
+      //      </div>
+      //     </div>
+      // )
+
       return (
-          <div className="row">
-          <div className="parallax"></div>
-          <div></div>
-           <div>
-           <SignupForm /><LoginForm />
-           <div className="bg-img2"></div><About />
-           <div className="bg-img5"></div>
-           </div>
-          </div>
+<div>
+   <div className="parallax">
+   <div className="caption">
+   </div>
+   </div>
+
+<div className="login">
+  <h3 className="login-h3">Parallax Demo</h3>
+  <p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+</div>
+
+<div className="bgimg-2">
+  <div className="caption">
+    <span className="border span1">LESS HEIGHT</span>
+  </div>
+</div>
+
+<div className="relative">
+  <div className="span2">
+    <p>Scroll up and down to really get the feeling of how Parallax Scrolling works.</p>
+  </div>
+</div>
+
+<div className="bgimg-3">
+  <div className="caption">
+    <span className="border span3">SCROLL UP</span>
+  </div>
+</div>
+
+<div className="relative">
+  <div className="span4">
+    <p>Scroll up and down to really get the feeling of how Parallax Scrolling works.</p>
+  </div>
+</div>
+
+<div className="bgimg-1">
+  <div className="caption">
+    <span className="border">COOL!</span>
+  </div>
+</div>
+</div>
       )
   }
 }
