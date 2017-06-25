a0854607b025b0ed8d4a1e15c476942a0aa4e032
sagerioz <ericaepperson27@yahoo.com>
2017-06-25 10:11:41 -0600

    slow link smooth scrolling installed

--- client/public/slab.js
+++ client/public/slab.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
-$("h1").slabText({
-            // Don't slabtext the headers if the viewport is under 380px
-            "viewportBreakpoint":380
+// $(document).ready(function() {
+// $("h1").slabText({
+//             // Don't slabtext the headers if the viewport is under 380px
+//             "viewportBreakpoint":380
+// });
+//
+// })
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 600);
 });
-})
