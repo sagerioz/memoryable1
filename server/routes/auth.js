be328b5a4b653014155e4b7f266f1d1a253e9324
sagerioz <ericaepperson27@yahoo.com>
2017-07-07 12:26:17 -0600

    cleaned up code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,27 +8,22 @@ const router = express.Router();
 
 
 router.post('/', (req, res) => {
-  console.log("IM IN THE AUTH POST ROUTE");
   const { identifier, password } = req.body;
-console.log("REQ BODY SERVER", req.body);
-if(identifier && password){
-knex('users')
-  .where('email', identifier)
-  .then(user => {
-    console.log("USER HERE?", user[0]);
+  if(identifier && password){
+    knex('users')
+    .where('email', identifier)
+    .then(user => {
     if (user) {
       const hash = user[0].password_digest
       const name = user[0].firstName
       const profilePicture = user[0].profilePicture
       const id = user[0].id
 
-
       if (bcrypt.compareSync(password, hash)) {
         const token = jwt.sign({
           id: user[0].id,
           username: user[0].userName
         }, jwtSecret);
-        console.log("TOKEN=================", token , name);
         res.json({ token: token , name: name, profilePicture: profilePicture, id:id });
       } else {
         res.status(401).json({ errors: { form: 'Invalid Credentials' } });
@@ -36,8 +31,8 @@ knex('users')
     } else {
       res.status(401).json({ errors: { form: 'Invalid Credentials' } });
     }
-  });
-}
+   });
+  }
 });
 
 
